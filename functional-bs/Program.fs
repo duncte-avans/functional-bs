4c74a76f97fa13f6685c511fd0e561e7dc0f4217
duncte123 <contact@duncte123.me>
2023-04-24 15:27:07 +0200

    Things

--- functional-bs/Program.fs
+++ functional-bs/Program.fs
@@ -1,4 +1,17 @@
-﻿type TrippleIndex =
+﻿// module functional_bs.Program
+
+// let add =
+//     fun a ->
+//         fun b ->
+//             a + b
+//             
+// add 1 3
+//
+// let alsoAdd x y = x + y
+//
+// alsoAdd 1 2
+
+type TrippleIndex =
     | First
     | Second
     | Third
@@ -11,34 +24,54 @@ type Square =
 type Row = Square * Square * Square
 type Board = Row * Row * Row
 
-let symbolOfSquare (s: Square) : string =
+let elementOfTriple (i: TrippleIndex) (tr: 'a * 'a * 'a): 'a =
+    match i, tr with
+    | First,  (r, _, _) -> r
+    | Second, (_, r, _) -> r
+    | Third,  (_, _, r) -> r
+
+let symbolOfSquare (s: Square) : string = // great method, wanna bake a potato?
     match s with
     | Empty -> " "
     | Circle -> "0"
     | Cross -> "X"
 
-let squareOfRow (index: TrippleIndex) (row: Row) : Square =
-    match index, row with
-    | First, (appelflap, _, _) -> appelflap
-    | Second, (_, pannenkoek, _) -> pannenkoek
-    | Third, (_, _, duikboot) -> duikboot
+// let squareOfRow (index: TrippleIndex) (row: Row) : Square =
+//     match index, row with
+//     | First, (appelflap, _, _) -> appelflap
+//     | Second, (_, pannenkoek, _) -> pannenkoek
+//     | Third, (_, _, duikboot) -> duikboot
 
-let rowOfBoard (index: TrippleIndex) (board: Board) : Row =
-    match index, board with
-    | First, (r, _, _) -> r
-    | Second, (_, r, _) -> r
-    | Third, (_, _, r) -> r
+// let rowOfBoard (index: TrippleIndex) (board: Board) : Row =
+//     match index, board with
+//     | First,  (r, _, _) -> r
+//     | Second, (_, r, _) -> r
+//     | Third,  (_, _, r) -> r
+    
+let stringOfRow (r: Row) : string =
+    $"| {symbolOfSquare (elementOfTriple First r)} | {symbolOfSquare (elementOfTriple Second r)} | {symbolOfSquare (elementOfTriple Third r)} |"
+    
+let stringOfBoard (b: Board): string =
+    $"-------------\n{stringOfRow ( elementOfTriple First b )}\n{stringOfRow ( elementOfTriple Second b )}\n{stringOfRow ( elementOfTriple Third b )}\n-------------"
+    
+// T and U are the old squares, and we replace it with the new square named S
+let updateSquareOfRow (i: TrippleIndex) (s: Square) (r: Row): Row =
+    match i, r with
+    | First,  (_, t, u) -> (s, t, u)
+    | Second, (t, _, u) -> (t, s, u)
+    | Third,  (t, u, _) -> (t, u, s)
 
-let row1: Row = Empty, Empty, Empty
-let row2: Row = Empty, Cross, Empty
-let row3: Row = Empty, Empty, Empty
+let board: Board =
+    (Circle, Cross, Circle),
+    (Cross, Empty, Cross),
+    (Circle, Cross, Circle)
 
-let board: Board = row1, row2, row3
-
-let row = rowOfBoard Second board
-let square = squareOfRow Second row
+let row = elementOfTriple Second board
+let square = elementOfTriple Second row
 
 let symbol = symbolOfSquare square
 
+let boardString = stringOfBoard board
+
 // For more information see https://aka.ms/fsharp-console-apps
-printfn $"{symbol}"
+printfn $"%s{boardString}"
